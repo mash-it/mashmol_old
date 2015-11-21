e661104d33abbb0dc6fffce97c6a4b55c332c09c
mash-it <itoh@theory.biophys.kyoto-u.ac.jp>
2015-11-21 16:26:32 +0900

    read ninfo file

--- mdrun.cc
+++ mdrun.cc
@@ -5,6 +5,34 @@
 
 using namespace std;
 
+class Bond
+{
+public:
+	int n1, n2;
+	float length; // bond length
+};
+
+class Angle
+{
+public:
+	int n1, n2, n3;
+	float angle; // bond angle
+};
+
+class Dihedral
+{
+public:
+	int n1, n2, n3, n4;
+	float dangle; // dihedral angle
+};
+
+class Contact
+{
+public:
+	int n1, n2;
+	float distance;
+};
+
 class MdSystem
 {
 	// position of particles
@@ -14,6 +42,11 @@ class MdSystem
 	vector<float> vx;
 	vector<float> vy;
 	vector<float> vz;
+	vector<Bond> bond;
+	vector<Angle> angle;
+	vector<Dihedral> dihedral;
+	vector<Contact> contact;
+
 public:
 	MdSystem() {}
 	void setNAtoms(int natoms) {
@@ -23,6 +56,12 @@ public:
 		vx.resize(natoms);
 		vy.resize(natoms);
 		vz.resize(natoms);
+	}
+	void setNForces(int b, int a, int d, int c) {
+		bond.resize(b);
+		angle.resize(a);
+		dihedral.resize(d);
+		contact.resize(c);
 	}
 	void setCoord(int i, float x, float y, float z) {
 		rx[i] = x;
@@ -35,45 +74,72 @@ int main(int argc, char *argv[]) {
 	std::string atomPath = "atom.dat";
 	std::string ninfoPath = "ninfo.dat";
 	std::string confPath = "conf.dat";
-
-	// open atom file
-	std::ifstream atomFile(atomPath.c_str());
-	if (atomFile.fail())
-	{
-		std::cerr << "failed to open " << atomPath << std::endl;
-		return 1;
-	}
-
-	std::string line;
-	std::string record;
-
-
-	// read number of atoms
-	int natoms = 0;
-	while (std::getline(atomFile, line)) {
-		record = util::removeSpaces(line.substr(0,8));
-		if (record == "ATOM") {
-			natoms++;
-		}
-	}
-	atomFile.clear();
-
+	std::string bufferLine, record;
 	MdSystem md;
-	md.setNAtoms(natoms);
 
-	// read atom coordinate
-	atomFile.seekg(0, ios_base::beg);
-	int i=0;
-	float x,y,z;
-	while (std::getline(atomFile, line)) {;
-		record = util::removeSpaces(line.substr(0,8));
-		if (record == "ATOM") {
-			x = stof(line.substr( 8,8));
-			y = stof(line.substr(16,8));
-			z = stof(line.substr(24,8));
-			md.setCoord(i,x,y,z);
-			i++;
+	// read atom file
+	{
+		std::ifstream atomFile(atomPath.c_str());
+		if (atomFile.fail())
+		{
+			std::cerr << "failed to open " << atomPath << std::endl;
+			return 1;
 		}
+	
+		// read number of atoms
+		int natoms = 0;
+		while (std::getline(atomFile, bufferLine)) {
+			record = util::removeSpaces(bufferLine.substr(0,8));
+			if (record == "ATOM") {
+				natoms++;
+			}
+		}
+		atomFile.clear();
+	
+		md.setNAtoms(natoms);
+	
+		// read atom coordinate
+		atomFile.seekg(0, ios_base::beg);
+		int i=0;
+		float x,y,z;
+		while (std::getline(atomFile, bufferLine)) {;
+			record = util::removeSpaces(bufferLine.substr(0,8));
+			if (record == "ATOM") {
+				x = stof(bufferLine.substr( 8,8));
+				y = stof(bufferLine.substr(16,8));
+				z = stof(bufferLine.substr(24,8));
+				md.setCoord(i,x,y,z);
+				i++;
+			}
+		}
+		atomFile.close();
 	}
-	atomFile.close();
+
+	// read force file
+	{
+		std::ifstream ninfoFile(ninfoPath.c_str());
+
+		int nbonds=0, nangles=0, ndiheds=0, nconts=0;
+		while (std::getline(ninfoFile, bufferLine)) {
+			record = util::removeSpaces(bufferLine.substr(0,8));
+			if (record == "BOND") nbonds++;
+			if (record == "ANGLE") nangles++;
+			if (record == "DIHED") ndiheds++;
+			if (record == "CONT") nconts++;
+		}
+		md.setNForces(nbonds, nangles, ndiheds, nconts);
+		cout << nbonds << " " << nangles << " " << ndiheds << " " << nconts << endl;
+		ninfoFile.clear();
+
+		ninfoFile.seekg(0, ios_base::beg);
+		int i=0;
+		float x,y,z;
+		while (std::getline(ninfoFile, bufferLine)) {;
+			record = util::removeSpaces(bufferLine.substr(0,8));
+			if (record == "BOND") {
+				
+			}
+		}
+		ninfoFile.close();
+	}
 }
